859f979766a643e3f567be3cfd4979a852a6df56
zakaharv <zaka.harvianto@gmail.com>
2023-11-06 16:57:28 +0700

    Add Example Data

---
+++ lib/home/domain/models/dummy_data.dart
@@ -0,0 +1,34 @@
+class Npwp {
+  Npwp({this.npwpNum = '', this.npwpOffice = '', this.npwpIsActive = false});
+
+  String npwpNum;
+  String npwpOffice;
+  bool npwpIsActive;
+}
+
+var listNpwp = [
+  Npwp(
+      npwpNum: '01.785.473.1-221.001',
+      npwpOffice: 'PT. BORWITA CITRA PRIMA 1',
+      npwpIsActive: true),
+  Npwp(
+      npwpNum: '01.785.473.1-221.002',
+      npwpOffice: 'PT. BORWITA CITRA PRIMA 2',
+      npwpIsActive: false),
+  Npwp(
+      npwpNum: '01.785.473.1-221.003',
+      npwpOffice: 'PT. BORWITA CITRA PRIMA 3',
+      npwpIsActive: true),
+  Npwp(
+      npwpNum: '01.785.473.1-221.004',
+      npwpOffice: 'PT. BORWITA CITRA PRIMA 4',
+      npwpIsActive: true),
+  Npwp(
+      npwpNum: '01.785.473.1-221.005',
+      npwpOffice: 'PT. BORWITA CITRA PRIMA 5',
+      npwpIsActive: true),
+  Npwp(
+      npwpNum: '01.785.473.1-221.006',
+      npwpOffice: 'PT. BORWITA CITRA PRIMA 6',
+      npwpIsActive: true),
+];
